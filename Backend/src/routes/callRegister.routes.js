b0e13cbc46c35180d587e6ab4cf4e366cf2669ec
Samuel Escobar <samuelescob200485@gmail.com>
2022-10-26 17:04:26 -0500

    callRegister getters added

--- Backend/src/routes/callRegister.routes.js
+++ Backend/src/routes/callRegister.routes.js
@@ -9,5 +9,8 @@ const mdAuth = require('../middlewares/authenticated');
 api.get('/testCallRegister', callRegisterController.testCallRegister);
 api.post('/startWorkingDay', mdAuth.ensureAuth, callRegisterController.startWorkingDay);
 api.post('/finishWorkingDay', mdAuth.ensureAuth, callRegisterController.finishWorkingDay);
+api.get('/getCallsToday', mdAuth.ensureAuth, callRegisterController.getCallsToday);
+api.get('/getCallsByDate', mdAuth.ensureAuth, callRegisterController.getCallsByDate);
+api.get('/getAllCalls', mdAuth.ensureAuth, callRegisterController.getAllCalls);
 
 module.exports = api;
